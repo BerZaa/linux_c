3e4f221282a5a4bd626e0c1044e056e5288235ee
BerZaa <3106598928@qq.com>
2021-10-29 22:39:59 +0800

    master

---
+++ signal/src/signal.c
@@ -0,0 +1,17 @@
+#include "signal.h"
+#include "stdio.h"
+#include "unistd.h"
+#include "sys/types.h"
+
+void *signal_handler() {
+    
+}
+
+int main(int argc, char *argv[]) {
+    pid_t current_pid;
+    current_pid = getpid();
+
+
+
+    return 0;
+}
